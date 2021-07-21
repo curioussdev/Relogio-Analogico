9978a3c51207eb27f93b39b39e58b49a525e3847
Jose Lopes <946393361bill@gmail.com>
2021-07-21 23:02:26 +0100

    digitalElement

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
 
 function updateClock() {
+    
+    const now = new Data();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    digitalElement.innerHTML = `${hour}:${minutes}:${seconds}`;
 
 };
 
+
+// de 1 em 1 segundo ele executará a função
 setInterval(updateClock, 1000);
