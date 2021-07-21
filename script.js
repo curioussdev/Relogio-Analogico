72c1683f87a3df8c99658e33bdc8f129aaa16fd9
Jose Lopes <946393361bill@gmail.com>
2021-07-21 22:25:39 +0100

    definindo setInterval e criando a função updateClock

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let digitalElement = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+
+};
+
+setInterval(updateClock, 1000);
