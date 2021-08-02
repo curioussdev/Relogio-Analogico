cd1ef49998caf0efc5fab29df5c1c7840a2f8879
Jose Lopes <946393361bill@gmail.com>
2021-08-02 20:39:25 +0100

    let sDeg posicionando o ponteiro de segundos para posição 0

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function updateClock() {
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
-    let sDeg = (360 / 6) * second;
+    let sDeg = ((360/60) * second) - 90;
 
-    sElement.style.transform = `rotate(${sDeg})deg`;
+    sElement.style.transform = `rotate(${sDeg}deg)`;
 };
 
 function fixZero(time) {
