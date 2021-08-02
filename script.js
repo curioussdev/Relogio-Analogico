c7d6c254466cf059d470a0efd661b940bce197da
Jose Lopes <946393361bill@gmail.com>
2021-08-02 20:11:11 +0100

    relógio digital finalizado

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let hElement = document.querySelector('.p_h');
 
 
 function updateClock() {
+    let now = new Date();
+    
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
 
+    digitalElement.innerHTML = `${hour}:${minute}:${second}`;
 };
 
 
