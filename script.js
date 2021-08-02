f58ad4cc73158e695de55f4274943c74c24f5846
Jose Lopes <946393361bill@gmail.com>
2021-08-02 20:04:42 +0100

    refatorando o relógio digital

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
 
-function updateClock() {
-    
-    const now = new Data();
-    let hour = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-
-    digitalElement.innerHTML = `${hour}:${minutes}:${seconds}`;
-
-};
 
 
 // de 1 em 1 segundo ele executará a função
-setInterval(updateClock, 1000);
